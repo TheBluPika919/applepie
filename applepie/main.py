a04127130c602eef40ab0f056f4d6b183195aeaa
JJ Gardner <jgardner+jj@jonathangardner.net>
2014-11-05 12:44:38 -0800

    Made 'This is great' yellow and moved it to the bottom.

--- applepie/main.py
+++ applepie/main.py
@@ -10,9 +10,10 @@ label1 = pyglet.text.Label('Apple Pie',
 
 label2 = pyglet.text.Label('This is great',
                           font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+                          font_size=72,
+                          color=(255, 255, 0, 255),
+                          x=window.width//2, y=0,
+                          anchor_x='center', anchor_y='bottom')
 @window.event
 def on_draw():
     window.clear()
