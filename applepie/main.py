1d3852ebadfd7e3d5de69d8b9574d487a3dcf864
Jonathan Gardner <jgardner@jonathangardner.net>
2015-02-07 12:00:09 -0800

    Added a logo

--- applepie/main.py
+++ applepie/main.py
@@ -1,6 +1,7 @@
 import pyglet
 
 window = pyglet.window.Window()
+image = pyglet.resource.image('Graphics/logo.png')
 
 label1 = pyglet.text.Label('Apple Pie',
                           font_name='Times New Roman',
@@ -14,10 +15,17 @@ label2 = pyglet.text.Label('This is great',
                           color=(255, 255, 0, 255),
                           x=window.width//2, y=0,
                           anchor_x='center', anchor_y='bottom')
+
+
+from pyglet.gl import *
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
 @window.event
 def on_draw():
     window.clear()
     label1.draw()
     label2.draw()
+    image.blit(window.width//2-image.width//2,window.height//2-image.width//2+20)
 
 pyglet.app.run()
